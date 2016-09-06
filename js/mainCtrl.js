d00ca009e657768d4c3b576bc464187f0fa93fb3
Brian Hudson <brianjasonhudson@gmail.com>
2016-09-06 13:41:55 -0500

    "Added input fields and button to add quotes"

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,5 +1,8 @@
 angular.module('quoteBookApp').controller('MainCtrl', function($scope, mainService) {
   $scope.quotes = mainService.getData();
-  $scope.addData = mainService.addData;
+  $scope.addData = function(quote) {
+    mainService.addData(quote);
+    $scope.quote = {};
+  }
   $scope.removeData = mainService.removeData;
 });
